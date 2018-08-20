c4d5eb9630d64410a517731ff1912064b34810d2
Moises P. Sena <moisespsena@gmail.com>
2018-08-20 18:57:02 -0300

    Update Plugin

--- plugin.go
+++ plugin.go
@@ -1,19 +1,20 @@
 package banner_editor
 
 import (
-	"github.com/moisespsena/go-assetfs/api"
 	"github.com/aghape/assets"
 	"github.com/aghape/db"
 	"github.com/aghape/plug"
+	"github.com/moisespsena/go-assetfs/api"
 )
 
 type Plugin struct {
-	db.DisDBNames
+	db.DBNames
+	plug.EventDispatcher
 	AssetFSKey string
 }
 
 func (p *Plugin) OnRegister() {
-	p.DBOnMigrateGorm(func(e *db.GormDBEvent) error {
+	db.Events(p).DBOnMigrateGorm(func(e *db.GormDBEvent) error {
 		return e.DB.AutoMigrate(&QorBannerEditorSetting{}).Error
 	})
 }
